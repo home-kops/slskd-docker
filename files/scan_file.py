130bb86c2ed810482464e1507832f899b96e54ef
msd117c <msd117c@gmail.com>
2025-11-03 20:54:57 +0100

    [build] Update script to scan dir

--- files/scan_file.py
+++ files/scan_file.py
@@ -10,30 +10,30 @@ completed_dir = os.environ["SLSKD_DOWNLOADS_DIR"]
 quarantine_dir = os.environ["QUARANTINE_DIR"]
 slskd_data = json.loads(os.environ['SLSKD_SCRIPT_DATA'])
 
-file_to_scan_path = slskd_data['localDirectoryName']
-print(f"File to scan: {file_to_scan_path}")
+directory_to_scan_path = slskd_data['localDirectoryName']
+print(f"Directory to scan: {directory_to_scan_path}")
 
-file_to_scan_name = os.path.basename(file_to_scan_path)
-print(f"File to scan name: {file_to_scan_name}")
+directory_to_scan_name = os.path.basename(directory_to_scan_path)
+print(f"Directory to scan name: {directory_to_scan_name}")
 
-log_file = f"{logs_dir}/{file_to_scan_name}_scan.log"
+log_file = f"{logs_dir}/{directory_to_scan_name}_scan.log"
 print(f"Log file: {log_file}")
 
 with open(log_file, "w") as f:
-    f.write(f"Scanning file: {file_to_scan_path}\n")
+    f.write(f"Scanning directory: {directory_to_scan_path}\n")
 
-subprocess.run(["/bin/sh", "-c", f"clamscan -ir --heuristic-alerts=yes --scan-archive=yes --max-filesize=2000M --max-scansize=2000M {file_to_scan_path}"], stdout=open(log_file, "a"), stderr=subprocess.STDOUT)
+subprocess.run(["/bin/sh", "-c", f"clamscan -ir --heuristic-alerts=yes --scan-archive=yes --max-filesize=2000M --max-scansize=2000M \"{directory_to_scan_path}\""], stdout=open(log_file, "a"), stderr=subprocess.STDOUT)
 
 with open(log_file, "r") as f:
     log_contents = f.read()
 
     if "Infected files: 0" in log_contents:
-        message = f"slskd scanned file {file_to_scan_name}: No infections found."
+        message = f"slskd scanned directory {directory_to_scan_name}: No infections found."
         target_dir = completed_dir
     else:
-        message = f"Quarantining file {file_to_scan_name} due to (potential) infection."
+        message = f"Quarantining directory {directory_to_scan_name} due to (potential) infection."
         target_dir = quarantine_dir
 
     print(message)
     subprocess.run(["/bin/sh", "-c", f"mail -s \"{message}\" \"{email_address}\" < \"{log_file}\""])
-    os.rename(file_to_scan_path, os.path.join(target_dir, file_to_scan_name))
+    os.rename(directory_to_scan_path, os.path.join(target_dir, directory_to_scan_name))
